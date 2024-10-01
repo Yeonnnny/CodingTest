098f3b5faebbe213cf11568097e6e5b1456f3db4
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2024-10-01 11:02:31 +0900

    [Silver IV] Title: 카드 놓기, Time: 36 ms, Memory: 31632 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,18 @@
+import itertools
+
+n = int(input())    # 총 카드의 수
+k = int(input())    # 선택할 카드의 수
+cards=[]            # 카드 번호 저장할 리스트
+result = []         # 조합을 저장할 리스트
+
+
+for i in range(n):  # 카드 번호 입력받기
+    cards.append(input())
+
+for permutated in itertools.permutations(cards,k):
+    val = ""
+    for p in permutated:
+        val+=p
+    result.append(val)
+
+print(len(set(result)))

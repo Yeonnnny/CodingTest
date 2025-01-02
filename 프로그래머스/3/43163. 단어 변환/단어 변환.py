badb0774b1427bc2d3894db2684682bc681481d8
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2025-01-02 16:20:52 +0900

    [level 3] Title: 단어 변환, Time: 0.71 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,31 @@
 from collections import deque
 
 def solution(begin, target, words):
-    
     if target not in words:
         return 0
     
-    # 한 글자 차이나는지 여부
-    def is_one_letter_diff(w1, w2):
-        diff_count = sum(1 for a, b in zip(w1, w2) if a!=b)
-        return diff_count == 1
+    # 한 글자만 다른지 여부
+    def is_one_letter_diff(w1,w2):
+        diff_count = 0
+        for a,b in zip(w1,w2):
+            if a!=b:
+                diff_count+=1
+        return 1 if diff_count == 1 else 0
     
-    # BFS
     queue = deque([(begin, 0)])
-    v = set()
-    
+    v = [0]*len(words)
+
+    # BFS 탐색
     while queue:
         current, steps = queue.popleft()
+
         if current == target :
             return steps
-        for word in words:
-            if word not in v and is_one_letter_diff(current, word):
-                v.add(word)
+        
+        for i, word in enumerate(words):
+            if not v[i] and is_one_letter_diff(current,word):
+                v[i] = 1
                 queue.append((word, steps+1))
+
+
     return 0
-    
-    answer = 0
-    return answer

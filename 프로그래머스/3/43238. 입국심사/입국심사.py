5721c93f97127b051315453907976afa652bdfe4
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2025-01-04 10:04:38 +0900

    [level 3] Title: 입국심사, Time: 575.45 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,22 +1,18 @@
 def solution(n, times):
     answer = 0
-    
     left = min(times)
-    right = max(times) * n
-    
-    while left <= right:
-        mid = (left+right) // 2
+    right = max(times)*n
+
+    while left <= right :
+        mid = (left+right)//2
         checked = 0
-        
+
         for time in times:
-            checked += mid // time
-            if checked >= n :
-                break
+            checked += mid//time
         
-        if checked >= n:
+        if checked >=n:
             answer = mid
             right = mid-1
-        elif checked < n:
-            left = mid+1        
-            
+        else :
+            left = mid+1
     return answer

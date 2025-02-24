6277774669d462de8804d0fb9726d22b3c08b43b
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2025-02-24 10:21:04 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,15 +1,10 @@
 def solution(s):
-    # 부호 판별: 음수이면 첫 문자가 '-', 양수이면 '+' 또는 숫자
-    is_negative = s[0] == '-'
-    # 부호 기호가 있으면 제외한 숫자 부분만 사용
-    digits = s[1:] if is_negative or s[0] == '+' else s
-
     answer = 0
-    power = 1  # 10의 거듭제곱을 누적할 변수
-
-    # 문자열의 마지막 문자부터 순회
-    for ch in reversed(digits):
-        answer += int(ch) * power
-        power *= 10
-
-    return -answer if is_negative else answer
+    
+    # for idx, number in enumerate(s[::-1]):
+    #     if number == '-':
+    #         answer *= (-1)
+    #     else:
+    #         answer += int(number) * (10**idx)
+    
+    return int(s)

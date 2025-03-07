64be8176438a2fb71522d2b5016b8a4aef4cb5f8
Yeonnnny <128607964+Yeonnnny@users.noreply.github.com>
2025-03-07 10:14:34 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,8 +1,8 @@
 def solution(seoul):
     answer = ''
     
-    for i, name in enumerate(seoul):
-        if name == "Kim":
-            answer = f'김서방은 {i}에 있다'
-    
-    return answer
+    # for i, name in enumerate(seoul):
+    #     if name == "Kim":
+    #         answer = f'김서방은 {i}에 있다'
+    # return answer
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
